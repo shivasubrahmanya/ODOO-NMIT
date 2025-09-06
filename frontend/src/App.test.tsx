b47d564476b60892c986a61e9ed5e1266a1c7c22
Shivasubrahmanya K C <shivakc004@gmail.com>
2025-09-06 18:40:40 +0530

    Delete frontend directory

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders SynergySphere app', () => {
-  render(<App />);
-  // Test that the app renders without crashing
-  expect(document.querySelector('.App')).toBeInTheDocument();
-});
